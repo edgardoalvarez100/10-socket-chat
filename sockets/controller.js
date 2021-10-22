9ff171fb1ee3fc239197a1daa36ee348a5ca30a1
Edgardo Alvarez <edgardo@livedesign.org>
2021-10-22 05:46:01 -0500

    add chat

--- sockets/controller.js
+++ sockets/controller.js
@@ -1,14 +1,15 @@
 const { Socket } = require("socket.io")
 const {comprobarJWT} = require("../helpers");
+const {ChatMensajes} = require("../models");
 
-
-const socketController = async(socket = new Socket) => {
+const socketController = async(socket = new Socket(), io ) => {
     const token = socket.handshake.headers['x-token'];
     const usuario = await comprobarJWT(token);
     if(!usuario){
         return socket.disconnect();
     }
-    console.log("Se conecto " + usuario.nombre)
+    ChatMensajes.conectarUsuario(usuario);
+    io.emit("usuarios-activos", ChatMensajes.usuariosArr)
 }
 
 module.exports = {socketController}
